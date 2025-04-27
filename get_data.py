1b7bd23176914b02395b2305953c4fe02a0f030f
junsangyooo <dbwnstkddb@gmail.com>
2025-04-27 13:22:52 +0900

    updated

--- get_data.py
+++ get_data.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 import csv
 import requests
 
@@ -20,15 +18,21 @@ REGION_CODE = {
     "e" : "e",
     "g" : "g"
 }
+region = "Yongsan"
 seoul_data_api_url = f"http://openapi.seoul.go.kr:8088/{API_KEY}/xml/LOCALDATA_072404/1/5/"
-specific_region_data_api_url = f"http://openapi.seoul.go.kr:8088/{API_KEY}/xml/LOCALDATA_072404{}/1/5/"
-
+specific_region_data_api_url = f"http://openapi.seoul.go.kr:8088/{API_KEY}/xml/LOCALDATA_072404{REGION_CODE[region]}/1/5/"
 res = requests.get(seoul_data_api_url)
 json_data = res.json()
 
-# with open('vaccin.csv', 'w', encoding='utf-8')as f:
-#     wr = csv.DictWriter(f, fieldnames = json_data['data'][0].keys())
-#     wr.writeheader()
-#     wr.writerows(json_data['data'])
-#df = pd.read_json(json_data)
-#df.to_csv('restaurant_data.csv', encoding='utf-8', index=False)
+with open('vaccin.csv', 'w', encoding='utf-8')as f:
+    wr = csv.DictWriter(f, fieldnames = json_data['data'][0].keys())
+    wr.writeheader()
+    wr.writerows(json_data['data'])
+df = pd.read_json(json_data)
+df.to_csv('restaurant_data.csv', encoding='utf-8', index=False)
+
+search_key_word = "한강로동맛집"
+naver_map_search_url = f"https://m.map.naver.com/search?query={search_key_word}"
+
+response = requests.get(naver_map_search_url)
+response.raise_for_status()
